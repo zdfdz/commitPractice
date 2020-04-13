e34240609daf1b29779d80004f48b1b18e172c49
zdfdz <zdfdz@outlook.com>
2020-04-13 20:44:28 +0800

    测试提交四
    测试提交五

--- app/src/main/java/study/zdf/testcommit/MainActivity.java
+++ app/src/main/java/study/zdf/testcommit/MainActivity.java
@@ -3,6 +3,7 @@ package study.zdf.testcommit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("zdf", "01");
+        Log.i("zdf", "02");
+        Log.i("zdf", "05");
     }
 }
